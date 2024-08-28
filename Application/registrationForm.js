188cef7deee5769b5736d9f3628e6af8184f2192
Anitta-Babu <anitta.babu@trenser.com>
2024-08-28 15:03:26 +0530

    Done review changes

---
+++ Application/registrationForm.js
@@ -0,0 +1,52 @@
+document
+  .getElementById("registrationFormDiv")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (emailValidation() && contactNumberValidation()) {
+      alert("Registration done successful");
+      location.replace("home.html");
+    } else {
+      alert("Registration not successful");
+    }
+  });
+
+function showPassword() {
+  const passwordElement = document.getElementById("Password");
+
+  if (passwordElement.type === "password") {
+    passwordElement.type = "text";
+  } else {
+    passwordElement.type = "password";
+  }
+}
+
+function emailValidation() {
+  const errorMessageElement = document.getElementById("emailErrorMessage");
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const emailElement = document.getElementById("Email").value;
+
+  if (emailPattern.test(emailElement)) {
+    errorMessageElement.textContent = "";
+    return true;
+  } else {
+    errorMessageElement.textContent = "Please enter a valid email address.";
+    return false;
+  }
+}
+
+function contactNumberValidation() {
+  const contactNumberErrorMessageElement = document.getElementById(
+    "contactNumberErrorMessage"
+  );
+  const contactNumberElement = document.getElementById("ContactNumber").value;
+
+  if (contactNumber.length === 10) {
+    contactNumberErrorMessageElement.textContent = "";
+    return true;
+  } else {
+    contactNumberErrorMessageElement.textContent =
+      "Please enter a valid phone number.";
+    return false;
+  }
+}
